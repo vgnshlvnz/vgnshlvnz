d511a5c84153a5423920c30be46b9e0e79a1c57b
Vigneshwaran Ravichandran <vgnshlvnz@users.noreply.github.com>
2021-01-23 21:30:56 +0800

    Update apache_startup.sh
    added comments

--- apache_startup.sh
+++ apache_startup.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+
+# Apache startup commands, to be used with eval command
 APACHESTATUS="apachectl status"
 APACHESTART="apachectl start"
 APACHERESTART="apachectl restart"
 APACHESTOP="apachectl stop"
 
+# Apache PID file path, to check if the PID file exist, if not, then start Apache
 APACHEPIDPATH="/var/run/httpd.pid"
 
+
+# if-else condition to check if the file exist, if it does then exit, otherwise start and exit.
 if [ -e $APACHEPIDPATH ]; then
         echo "PID file exist! Nothing to do."
 
